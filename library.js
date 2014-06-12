10b4e751586f4fae4d77587c28fe8b3b43fd4c08
a5mith <aaronsmith91@sky.com>
2014-06-12 19:17:55 +0100

    Changes to iFrame Style
    removed borders

--- library.js
+++ library.js
@@ -2,7 +2,7 @@
     "use strict";
 
     var Slideshare = {},
-        embed = '<iframe src="http://www.slideshare.net/slideshow/embed_code/$1" width="427" height="356" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px 1px 0; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe>'
+        embed = '<iframe src="http://www.slideshare.net/slideshow/embed_code/$1" width="427" height="356" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe>'
 
 
     Slideshare.parse = function(postContent, callback) {
